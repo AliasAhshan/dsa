8508d954aa33bd132345bc6b5bacff63f55241cd
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-10-09 14:10:00 -0400

    Learned and practiced multiple pointers pattern

--- MultiplePointers.js
+++ MultiplePointers.js
@@ -7,3 +7,28 @@ sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
 sumZero([-2,0,1,3]) // undefined
 sumZero([1,2,3]) // undefined
 */
+
+// function sumZero(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; i++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]]
+//       }
+//     }
+//   }
+// }
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
