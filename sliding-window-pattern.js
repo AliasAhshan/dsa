df8fba3d52be7089d8c3738d77b23bde22cead0a
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-10-11 12:08:00 -0400

    learned and practiced sliding window pattern

---
+++ sliding-window-pattern.js
@@ -0,0 +1,48 @@
+/*
+Write a function called maxSubarraySum which accepts an array of integers and a number called n.
+The function should calculate the maximum sum of n consecutive elements in the array.
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+maxSubarraySum([4,2,1,6],1) // 6
+maxSubarraySum([4,2,1,6,2],4) // 13
+maxSubarraySum([],4) // null
+*/
+
+// function maxSubArraySum(arr, num) {
+//   if (num > arr.length) {
+//     null;
+//   }
+
+//   var max = -Infinity;
+//   for (let i = 0; i < arr.legnth - num; i++) {
+//     sum = 0
+//     for (let j = 0; j < num; j++){
+
+//       sum += arr[i + j]
+//     }
+//     if (sum > max) {
+//       max = sum
+//     }
+//   }
+//   return max
+// }
+
+function MaxSubArraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  if (arr.length < num) return null;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
