d635a8965ed1f06873d3291067df309062e1c98b
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-11-10 13:26:58 -0500

    More recursion and pure recursion

---
+++ recursion/pure.js
@@ -0,0 +1,59 @@
+function collectOddValues(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) {
+    return newArr;
+  }
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValue(arr.slice(1)));
+  return newArr;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5]));
+
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat(collectOddValues[4,5])
+//[].concat(collectOddValues[5])
+//[5].concat(collectOddValues[])
+//[]
+
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat(collectOddValues[4,5])
+//[].concat(collectOddValues[5])
+//[5].concat(collectOddValues[])
+//[]
+
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat(collectOddValues[4,5])
+//[].concat(collectOddValues[5])
+//[5].concat([])
+
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat(collectOddValues[4,5])
+//[].concat(collectOddValues[5])
+//[5]
+
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat(collectOddValues[4,5])
+//[].concat([5])
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat([5])
+
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3].concat(collectOddValues[4,5])
+//[].concat([5])
+//[1].concat(collectOddValues[2,3,4,5])
+//[].concat(collectOddValues[3,4,5])
+//[3,5]
+
+//and so on
