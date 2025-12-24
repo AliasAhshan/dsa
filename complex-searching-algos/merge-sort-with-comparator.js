1c11e829140fed168bb8a04cce6bcd8d99a373bc
AliasAhshan <ahshan.ahmed003@gmail.com>
2025-12-24 14:08:49 -0500

    Learned and practiced merge sort

---
+++ complex-searching-algos/merge-sort-with-comparator.js
@@ -0,0 +1,39 @@
+function merge(arr1, arr2, comparator) {
+  if (typeof comparator !== 'function') {
+    comparator = (a, b) => a - b;
+  }
+
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (comparator(arr2[j], arr1[i]) > 0) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  return results;
+}
+
+function mergeSort(arr, comparator) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid), comparator);
+  let right = mergeSort(arr.slice(mid), comparator);
+  return merge(left, right, comparator);
+}
