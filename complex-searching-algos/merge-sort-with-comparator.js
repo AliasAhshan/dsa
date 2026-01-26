8bb93913af9dd6a20558801ca84fe4030524321b
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-01-26 15:02:54 -0500

    Learned and practiced quick sort

--- complex-searching-algos/merge-sort-with-comparator.js
+++ complex-searching-algos/merge-sort-with-comparator.js
@@ -1,14 +1,51 @@
-function merge(arr1, arr2, comparator) {
-  if (typeof comparator !== 'function') {
-    comparator = (a, b) => a - b;
-  }
+// function merge(arr1, arr2, comparator) {
+//   if (typeof comparator !== 'function') {
+//     comparator = (a, b) => a - b;
+//   }
 
+//   let results = [];
+//   let i = 0;
+//   let j = 0;
+
+//   while (i < arr1.length && j < arr2.length) {
+//     if (comparator(arr2[j], arr1[i]) > 0) {
+//       results.push(arr1[i]);
+//       i++;
+//     } else {
+//       results.push(arr2[j]);
+//       j++;
+//     }
+//   }
+
+//   while (j < arr2.length) {
+//     results.push(arr2[j]);
+//     j++;
+//   }
+
+//   while (i < arr1.length) {
+//     results.push(arr1[i]);
+//     i++;
+//   }
+
+//   return results;
+// }
+
+// function mergeSort(arr, comparator) {
+//   if (arr.length <= 1) return arr;
+//   let mid = Math.floor(arr.length / 2);
+//   let left = mergeSort(arr.slice(0, mid), comparator);
+//   let right = mergeSort(arr.slice(mid), comparator);
+//   return merge(left, right, comparator);
+// }
+
+function merge(arr1, arr2) {
   let results = [];
-  let i = 0;
+
   let j = 0;
+  let i = 0;
 
   while (i < arr1.length && j < arr2.length) {
-    if (comparator(arr2[j], arr1[i]) > 0) {
+    if (arr2[j] > arr1[i]) {
       results.push(arr1[i]);
       i++;
     } else {
@@ -17,23 +54,25 @@ function merge(arr1, arr2, comparator) {
     }
   }
 
-  while (j < arr2.length) {
-    results.push(arr2[j]);
-    j++;
-  }
-
   while (i < arr1.length) {
     results.push(arr1[i]);
     i++;
   }
 
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
   return results;
 }
 
-function mergeSort(arr, comparator) {
+function mergeSort(arr) {
   if (arr.length <= 1) return arr;
   let mid = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0, mid), comparator);
-  let right = mergeSort(arr.slice(mid), comparator);
-  return merge(left, right, comparator);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
 }
+
+console.log(mergeSort([2, 4, 5, 6, 1, 3, 4, 7]));
---
+++ complex-searching-algos/quick-sort.js
@@ -0,0 +1,31 @@
+function pivot(arr, start = 0, end = arr.length - 1) {
+  function swap(array, index1, index2) {
+    [array[index1], array[index2]] = [array[index2], array[index1]];
+  }
+
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+  return swapIndex;
+}
+console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
