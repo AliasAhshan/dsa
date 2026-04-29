cc38c8d4495d685b92c636de70b2fa071e6ae6f5
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-04-29 11:35:27 -0400

    Started learning SLL and implemented push and pop functions

---
+++ sll/singly-linked-list.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var list = new SinglyLinkedList();
+console.log(list.push('HELLO'));
+console.log(list.push('YOU'));
+
+// var first = new Node("Hi")
+// first.next = new Node("Bye")
+// first.next.next = new Node("Hello")
