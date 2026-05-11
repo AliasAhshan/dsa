a0ce46f555a81b33d522e1129a2d7aee8c188960
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-05-11 12:23:33 -0400

    Learned and implemented stack via SLL and array

---
+++ stacks-queues/stacks.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return temp.value;
+  }
+}
+
+var stack = new Stack();
+
+console.log(
+  stack.push('1'),
+  stack.push('2'),
+  stack.pop(),
+  stack.pop(),
+  stack.pop(),
+);
