6dc3518f0cbffd1b6280ba945e959e6bd2624bb9
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-06-02 13:13:03 -0400

    learned all about graph theory and implemented a graph class with addvertex, edge and remove vertex edge

---
+++ graphs/graphs.js
@@ -0,0 +1,39 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    //to make this directed you wouldnt do both ways just one way
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+let g = new Graph();
+g.addVertex('San Francisco');
+g.addVertex('Tokyo');
+g.addVertex('Dallas');
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Dallas', 'San Francisco');
+g.removeEdge('Dallas', 'San Francisco');
+g.removeVertex('Dallas');
+
+console.log(g);
